20d193125504043feb51b11cceed17fe2d7f0f6d
Midlaj Marutha <midlajmarutha4@gmail.com>
2024-09-07 14:18:01 +0530

    updation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,26 @@
-const menu = document.getElementById('mobileMenu')
+const menu = document.getElementById("mobileMenu");
 
-
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
         e.preventDefault();
-        menu.classList.add("hidden")
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
+        menu.classList.add("hidden");
+        // document.querySelector(this.getAttribute("href")).scrollIntoView({
+        //     behavior: "smooth",
+        //     offset: "100px",
+        // });
+        var element = document.querySelector(this.getAttribute("href"));
+        var headerOffset = 45;
+        var elementPosition = element.getBoundingClientRect().top;
+        var offsetPosition =
+            elementPosition + window.scrollY - headerOffset;
+
+        window.scrollTo({
+            top: offsetPosition,
+            behavior: "smooth",
         });
     });
 });
 
-document.getElementById('navToggle').addEventListener('click', function() {
-    menu.classList.toggle('hidden');
+document.getElementById("navToggle").addEventListener("click", function () {
+    menu.classList.toggle("hidden");
 });
